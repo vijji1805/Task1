fbe4155ee386d52268b9bf69a826476308a21c2a
vijayavadlamudi <vadlamudivijaya21@gmail.com>
2020-07-09 06:02:46 +0000

    region changed

--- instances.sh
+++ instances.sh
@@ -1,4 +1,4 @@
 for ID in $(aws autoscaling describe-auto-scaling-instances --region us-east-1 --query AutoScalingInstances[].InstanceId --output text);
 do
-aws ec2 describe-instances --instance-ids $ID --region us-east-1 --query Reservations[].Instances[].PublicIpAddress --output text
+aws ec2 describe-instances --instance-ids $ID --region ap-south-1 --query Reservations[].Instances[].PublicIpAddress --output text
 done
